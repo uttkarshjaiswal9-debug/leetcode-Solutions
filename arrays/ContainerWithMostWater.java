218ac26ebd05afb41c503724a4f825fec97301ba
uttkarshjaiswal9-debug <uttkarshjaiswal9@gmail.com>
2026-09-14 12:47:44 +0530

    Added:Container with most water

---
+++ arrays/ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentArea = width * currentHeight;
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        ContainerWithMostWater solution = new ContainerWithMostWater();
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(solution.maxArea(height));
+    }
+}
